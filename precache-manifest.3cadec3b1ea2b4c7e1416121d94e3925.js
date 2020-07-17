d78f1b7568b358d1a46e01180eb0980c6476b822
Xhydracore <sutrisnodeyndrawan@gmail.com>
2020-07-17 16:55:46 +0700

    Updates

---
+++ precache-manifest.3cadec3b1ea2b4c7e1416121d94e3925.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbacc531856694cad0589ec2f953c10",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "4d59afea402ce9ba95aa",
+    "url": "/Ravenous/static/css/main.ae32d455.chunk.css"
+  },
+  {
+    "revision": "9925d90d1471d1ce66f7",
+    "url": "/Ravenous/static/js/2.99d41395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ravenous/static/js/2.99d41395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d59afea402ce9ba95aa",
+    "url": "/Ravenous/static/js/main.4a1e7eab.chunk.js"
+  },
+  {
+    "revision": "cd33480439a159f358bd",
+    "url": "/Ravenous/static/js/runtime-main.9bb2f265.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
